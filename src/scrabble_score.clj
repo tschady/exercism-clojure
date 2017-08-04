a6bec63668c658115c2a0c0349e81e3b89bde3f3
tschady <tschady@users.noreply.github.com>
2017-08-04 09:04:53 -0500

    Solve scrabble score

---
+++ src/scrabble_score.clj
@@ -0,0 +1,39 @@
+(ns scrabble-score
+  (:require [clojure.string :as str]))
+
+(def char->score {\A 1
+                  \B 3
+                  \C 3
+                  \D 2
+                  \E 1
+                  \F 4
+                  \G 2
+                  \H 4
+                  \I 1
+                  \J 8
+                  \K 5
+                  \L 1
+                  \M 3
+                  \N 1
+                  \O 1
+                  \P 3
+                  \Q 10
+                  \R 1
+                  \S 1
+                  \T 1
+                  \U 1
+                  \V 4
+                  \W 4
+                  \X 8
+                  \Y 4
+                  \Z 10})
+
+(defn score-word
+  "Produce Scrabble score for given string."
+  [s]
+  (->> (str/upper-case s)
+       (map char->score)
+       (reduce + 0)))
+
+; unnecessary function, but specified in the unit tests
+(def score-letter score-word)
